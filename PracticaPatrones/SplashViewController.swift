9b37850efa59033e36e5f18fbf2292652b07911c
Roberto Rojo <rrojo.va@gmail.com>
2022-09-19 17:02:18 +0200

    Try to change between screen with instantiateController -> without iniatialController

--- PracticaPatrones/SplashViewController.swift
+++ PracticaPatrones/SplashViewController.swift
@@ -14,6 +14,8 @@ class SplashViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     //MARK: Constants
+    
+    let storyboardName = "HomeView"
 
     
     //MARK: Life Cycle
@@ -41,11 +43,14 @@ class SplashViewController: UIViewController {
         //Create new thread in main thread after now + 3 seconds and do the clousure -> navigate next view
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
             // TODO: navigate next view
-            let homeStoryboard = UIStoryboard(name: "HomeView", bundle: nil)
-                
-                guard let destinationViewController = homeStoryboard.instantiateInitialViewController() else { return }
-                
-                self.navigationController?.setViewControllers([destinationViewController], animated: false)
+            let homeStoryboard = UIStoryboard(name: self.storyboardName, bundle: nil)
+//            guard let destinationViewController = homeStoryboard.instantiateInitialViewController() else { return }
+            
+            //How & Why to do it without initialController option
+            guard let destinationViewController = homeStoryboard.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController else { return }
+            
+            
+                self.navigationController?.setViewControllers([destinationViewController], animated: true)
                 
         }
     }
