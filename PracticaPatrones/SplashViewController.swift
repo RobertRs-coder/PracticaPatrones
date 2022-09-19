fd5a92d41e33eb3ee953840b2603e240a175f27c
Roberto Rojo <rrojo.va@gmail.com>
2022-09-19 12:56:27 +0200

    Add assets & Create Splash

---
+++ PracticaPatrones/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaPatrones
+//
+//  Created by Roberto Rojo Sahuquillo on 19/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
